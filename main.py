23497b45d598f3c4d3c102d5e5cd5dc9835a300a
bfc782 <bfc782@hotmail.com>
2024-06-04 14:28:29 +0200

    refactor px.line into components

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from dash import Dash, callback, Output, Input
-import plotly.express as px
+from dash import Dash
 
+from app.callbacks import get_callbacks
+from app.components import Components
 from app.data import df_fake_data as data
 from app.layout import get_layout
 
@@ -11,19 +12,10 @@ def serve_layout():
     layout = get_layout(data)
     return layout
 
+components = Components(data)
 app.layout = serve_layout
 
-@callback(
-    Output('graph-content', 'figure'),
-    Input('dropdown-selection', 'value')
-)
-def update_graph(value):
-
-    dff = data[data.customer==value]
-    fig = px.line(dff, x='transaction_date', y='amount')
-    fig.update_xaxes(type='date', tickformat='%Y-%m-%d', tickvals=dff['transaction_date'])
-    fig.update_yaxes(range=[0, 200])
-    return fig
+_ = get_callbacks(components)
 
 if __name__ == '__main__':
     app.run(debug=True)
