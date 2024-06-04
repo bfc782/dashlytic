d979751efb8c15eed3852382d09bcd1555db2764
bfc782 <bfc782@hotmail.com>
2024-06-04 12:22:01 +0200

    refactor in folders and formatted with ruff

---
+++ app/data.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from datetime import date, timedelta
+from random import sample
+
+fake_data = [
+    {"amount": 100, "customer": "abc", "transaction_date": "2024-05-31"},
+    {"amount": 120, "customer": "abc", "transaction_date": "2024-05-30"},
+    {"amount": 105, "customer": "abc", "transaction_date": "2024-05-29"},
+    {"amount": 110, "customer": "abc", "transaction_date": "2024-05-28"},
+    {"amount": 75, "customer": "abc", "transaction_date": "2024-05-27"},
+    {"amount": 120, "customer": "def", "transaction_date": "2024-05-31"},
+    {"amount": 130, "customer": "def", "transaction_date": "2024-05-30"},
+    {"amount": 140, "customer": "def", "transaction_date": "2024-05-29"},
+    {"amount": 120, "customer": "def", "transaction_date": "2024-05-28"},
+    {"amount": 120, "customer": "def", "transaction_date": "2024-05-27"},
+    {"amount": 120, "customer": "ghi", "transaction_date": "2024-05-31"},
+    {"amount": 150, "customer": "ghi", "transaction_date": "2024-05-30"},
+    {"amount": 150, "customer": "ghi", "transaction_date": "2024-05-29"},
+    {"amount": 140, "customer": "ghi", "transaction_date": "2024-05-28"},
+    {"amount": 150, "customer": "ghi", "transaction_date": "2024-05-27"},
+]
+
+df_fake_data = pd.DataFrame.from_records(fake_data)
